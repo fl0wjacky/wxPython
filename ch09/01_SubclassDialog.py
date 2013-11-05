6087ee53f2bf0cacea6433b2c25003c5ac1f917c
fl0wjacky <flowjacky@gmail.com>
2013-11-05 08:28:01 +0800

    Start ch09

---
+++ ch09/01_SubclassDialog.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+import wx
+
+class SubclassDialog(wx.Dialog):
+    def __init__(self): # 初始化对话框
+        wx.Dialog.__init__(self,None,-1,"Dialog Subclass",size=(300,100))
+        okButton = wx.Button(self,wx.ID_OK,"OK",pos = (15,15))
+        okButton.SetDefault()
+        cancelButton = wx.Button(self,wx.ID_CANCEL,"Cancel",pos = (115,15))
+
+if __name__ == "__main__":
+    app = wx.PySimpleApp()
+    app.MainLoop()
+    dialog = SubclassDialog()
+    result = dialog.ShowModal() # 显示模式对话框
+    if result == wx.ID_OK:
+        print "OK"
+    else:
+        print "Cancel"
+    dialog.Destroy()
