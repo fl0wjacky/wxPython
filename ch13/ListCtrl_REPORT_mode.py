a817aca6d03825389faf4ba31091523373a4ecb5
fl0wjacky <flowjacky@gmail.com>
2013-12-06 14:15:15 +0800

    add files

---
+++ ch13/ListCtrl_REPORT_mode.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+import wx
+import sys,glob,random
+import data
+
+class DemoFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, -1, 'wx.ListCtrl in wx.LC_REPORT mode', size = (600, 400))
+        il = wx.ImageList(16, 16, True)
+        for name in glob.glob('smicon??.png'):
+            bmp = wx.Bitmap(name, wx.BITMAP_TYPE_PNG)
+            il_max = il.Add(bmp)
+        self.list = wx.ListCtrl(self, -1, style = wx.LC_REPORT)
+        self.list.AssignImageList(il, wx.IMAGE_LIST_SMALL)
+
+        for col, text in enumerate(data.columns):
+            self.list.InsertColumn(col, text)
+
+        for item in data.rows:
+            index = self.list.InsertStringItem(sys.maxint,item[0])
+            for col, text in enumerate(item[1:]):
+                self.list.SetStringItem(index,col+1, text)
+
+            img = random.randint(0, il_max)
+            self.list.SetItemImage(index, img, img)
+        
+        self.list.SetColumnWidth(0, 120)
+        self.list.SetColumnWidth(1, wx.LIST_AUTOSIZE)
+        self.list.SetColumnWidth(2, wx.LIST_AUTOSIZE)
+        self.list.SetColumnWidth(3, wx.LIST_AUTOSIZE_USEHEADER)
+
+class App(wx.App):
+    def OnInit(self):
+        self.f = DemoFrame()
+        self.f.Show()
+        return True
+
+if __name__ == "__main__":
+    App().MainLoop()
---
+++ ch13/data.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+columns = ['Request ID', 'Summary', 'Date', 'Name']
+rows = [
+    ['a','additions to RTTP', '2004-07-08', 'Lucy'],
+    ['b','wxTextCtrl-disable auto-scrolling', '2003-11-20', 'Rose'],
+    ['c','xxxxxxxxxx', '2012-12-21', 'Tank'],
+    ]
